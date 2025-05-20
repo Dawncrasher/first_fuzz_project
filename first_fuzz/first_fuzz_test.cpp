73c8e8c8c9081e5d3986e0ed0375e75e0f00c171
Johannes Lang <johannes.lang@phios.group>
2025-05-20 10:12:31 +0200

    added example, updated gitignore

--- first_fuzz/first_fuzz_test.cpp
+++ first_fuzz/first_fuzz_test.cpp
@@ -1,7 +1,19 @@
 #include "fuzztest/fuzztest.h"
 #include "gtest/gtest.h"
+#include <cmath>
 
 TEST(MyTestSuite, OnePlustTwoIsTwoPlusOne) { EXPECT_EQ(1 + 2, 2 + 1); }
 
 void IntegerAdditionCommutes(int a, int b) { EXPECT_EQ(a + b, b + a); }
+void IntegerDivison(int a, int b) {
+
+  int result = INFINITY;
+  if (b == 0) {
+    return;
+  }
+  result = a / b;
+  // EXPECT_EQ(a / b, result);
+}
+
 FUZZ_TEST(MyTestSuite, IntegerAdditionCommutes);
+FUZZ_TEST(MyTestSuite, IntegerDivison);
